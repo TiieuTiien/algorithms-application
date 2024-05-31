f8482fb2b8137d7ac426b09113c65a694dab797b
Phạm Quốc Tiến <106142689+TiieuTiien@users.noreply.github.com>
2024-05-31 14:49:05 +0700

    implement Z algorithm

--- Hand-On-07/bai05_thuatToanXuLyXauKyTu.cpp
+++ Hand-On-07/bai05_thuatToanXuLyXauKyTu.cpp
@@ -63,7 +63,7 @@ bool Boyer_Moore_Horspool(char P[], char T[]) {
 }
 
 //Thuat toan Z
-void z_algo(const char * s, int * z) {
+void z_algo(const char *s, int *z) {
     int n = strlen(s), left = 0, right = 0;
     for (int i = 1; i < n; i++) {
         if (i > right) {
@@ -72,7 +72,7 @@ void z_algo(const char * s, int * z) {
                 right++;
             z[i] = right - left;
             right--;
-        } else if (z[i - left] < right– i + 1)
+        } else if (z[i - left] < right - i + 1)
             z[i] = z[i - left];
         else {
             left = i;
@@ -84,20 +84,46 @@ void z_algo(const char * s, int * z) {
     }
 }
 
+//Xau con chung dai nhat
+int xauConChungDaiNhat(const char *L) {
+    for (int i = 0; i <= len(A); i++) L[i][0] = 0;
+    for (int j = 0; j <= len(B); j++) L[0][j] = 0;
+    for (int i = 1; i <= len(A); i++)
+        for (int j = 1; j <= len(B); j++)
+            if (A[i - 1] == B[j - 1])
+                L[i][j] = L[i - 1][j - 1] + 1;
+            else
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+    return L[m][n];
+}
+
 int main(){
 	string s = "abccabdad123";
-	char P[] = "daddyissue";
-	char T[] = "ihavedaddyissue";
-	
 	cout << "Dem so lan xuat hien cac ky tu trong sau ky tu:\n";
 	countDistinct(s);
 	
+	char P[] = "daddyissue";
+	char T[] = "ihavedaddyissue";
 	cout << "P co phai xau con cua T khong?\n";
-	if (indexOf(P, T) == -1) cout << "Khong\n";
-	else cout << "Co\n";
+	if (indexOf(P, T) == -1) cout << "Khong\n\n";
+	else cout << "Co\n\n";
 	
-	if (Boyer_Moore_Horspool(P, T)) cout << "Co\n";
-	else cout << "Khong\n";
+	cout << "P co phai xau con cua T khong? (Boyer Moore Horspool)\n";
+	if (Boyer_Moore_Horspool(P, T)) cout << "Co\n\n";
+	else cout << "Khong\n\n";
+	
+	char S[] = "ACBDABACBAC";
+	int length = sizeof(S)/sizeof(char);
+	int z[length] = {-1};
+
+	cout << "Z algorithm:\n";
+	z_algo(S, z);
+	for (int i = 0; i < length; i++){
+		cout << z[i] << " ";
+	}
+	cout << "\n";
+	
+	
 	
 	return 0;
 }
